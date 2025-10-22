f3caa1c8598bd1ca1d7ef6d6b16ee31b574a8b4a
xiaogf <1021969874@qq.com>
2025-10-22 09:52:33 +0800

    chore: 更新解包脚本中的项目路径
    将WXAPPUNPACKER_PATH从F盘更改为D盘以匹配当前项目位置

--- unpack.bat
+++ unpack.bat
@@ -2,7 +2,7 @@
 # date: 2019/03/16
 
 # WXAPPUNPACKER_PATH 项目路径
-set WXAPPUNPACKER_PATH=F:\GitStore\wxappUnpacker
+set WXAPPUNPACKER_PATH=D:\GitStore\wxappUnpacker
 
 set FILE_FORMAT=wxapkg
 
@@ -22,7 +22,7 @@ set de_dir=%1
 
       set de_dir=%WXAPPUNPACKER_PATH%
 
-echo %de_dir%
+echo %de_dir% 
 
 echo "for wxapkg in `find %de_dir% "*.${FILE_FORMAT}"`"
 
